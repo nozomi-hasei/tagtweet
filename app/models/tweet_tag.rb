6a31a3ccc879f1dce78de1bea3423d694db04409
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2021-04-27 10:39:34 +0900

    修正

--- app/models/tweet_tag.rb
+++ app/models/tweet_tag.rb
@@ -10,8 +10,9 @@ class TweetsTag
 
   def save
     tweet = Tweet.create(message: message)
-    tag = Tag.create(name: name)
+    tag = Tag.where(name: name).first_or_initialize
+    tag.save
 
     TweetTagRelation.create(tweet_id: tweet.id, tag_id: tag.id)
-    end
+  end
 end
